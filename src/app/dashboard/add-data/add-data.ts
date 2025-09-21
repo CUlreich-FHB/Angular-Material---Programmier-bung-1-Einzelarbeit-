aad274d851cfb4c3a8f3116045d08fe705214a72
Julia Schabasser <julia.schabasser@fsw.at>
2025-09-21 11:41:19 +0200

    Initial commit

---
+++ src/app/dashboard/add-data/add-data.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-data',
+  imports: [],
+  templateUrl: './add-data.html',
+  styleUrl: './add-data.scss'
+})
+export class AddData {
+
+}
